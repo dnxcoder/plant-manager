c378cd9f3e339efdec73aa26b376a343f7f9a5e4
DnxCoder <dnxcoder@gmail.com>
2021-04-20 18:59:25 -0300

    Page Welcome Done

---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView} from 'react-native';
+
+export function UserIdentification(){
+
+
+
+    return(
+        <SafeAreaView style={styles.container}>
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    container: {}
+})
