fb13ef87dde1a46cad78717642a788285040bc75
dnxcoder <dnxcoder@gmail.com>
2021-04-20 21:27:21 -0300

    Working on PlayerNamePage

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,18 +1,98 @@
 import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TextInput,
+    KeyboardAvoidingView,
+    Platform
+} from 'react-native';
+import colors from '../../assets_aula01/styles/colors';
+import fonts from '../styles/fonts';
 
-export function UserIdentification(){
+import { Button } from '../components/Button';
+
+export function UserIdentification() {
 
 
 
-    return(
+    return (
         <SafeAreaView style={styles.container}>
+            <KeyboardAvoidingView
+                style={styles.container}
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            >
+                <View style={styles.container}>
+                    <View style={styles.form}>
+                        <Text style={styles.emoji}>
+                            😄
+                    </Text>
+                        <Text style={styles.title}>
+                            Como podemos {'\n'}
+                        chamar você?
+                    </Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Digite um nome"
+                        />
+                        <View style={styles.footer}>
+                            <Button />
+                        </View>
 
+                    </View>
+                </View>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
 
-    container: {}
+    container: {
+
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    content: {
+
+        flex: 1,
+        width: '100%'
+    },
+    form: {
+        flex: 1,
+        justifyContent: 'center',
+        width: '75%',
+        alignItems: 'center',
+
+    },
+    emoji: {
+        fontSize: 44
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderColor: colors.gray,
+        color: colors.heading,
+        width: '100%',
+        fontSize: 18,
+        marginTop: 50,
+        padding: 10,
+        textAlign: 'center',
+    },
+    title: {
+        fontSize: 24,
+        lineHeight: 32,
+        textAlign: 'center',
+        fontFamily: fonts.heading,
+        marginTop: 20
+    },
+    footer: {
+        width: '100%',
+        marginTop: 40,
+        paddingHorizontal: 20,
+
+    }
+
 })
